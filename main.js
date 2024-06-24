18348ce53dbb983fdf17245d0a5ebe618c297009
sodexkings <71529813+sodexkings@users.noreply.github.com>
2024-06-24 18:55:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.hide();
+}
+
+
+function draw()
+{
+image(video, 0, 0, 300, 300)
+}
+
+function take_snapshot()
+{
+    save('savePicture.png')
+}
